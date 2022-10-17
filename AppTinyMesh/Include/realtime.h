1fc15775a96c55d25dfe7afaa133d9e09fa5dcdf
Helsinki <69796745+He1sinki@users.noreply.github.com>
2022-10-17 17:01:17 +0200

    Revert "Fix compilation for Qt5"
    This reverts commit a8ab897be8c24967e9b00f038f5a597b9a7a042c.

--- AppTinyMesh/Include/realtime.h
+++ AppTinyMesh/Include/realtime.h
@@ -169,9 +169,6 @@ public:
   void SetShading(const QString&, MeshShading);
   void SetShadingGlobal(MeshShading);
 
-private:
-  void _InternalGetMouseGlobalPosition(QMouseEvent* e, int& x0, int& y0) const;
-
 protected:
   virtual void initializeGL();
   virtual void resizeGL(int, int);
